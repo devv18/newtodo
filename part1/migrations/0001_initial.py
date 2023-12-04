628698e9db677cd77f9a78eda854299437327f40
devv18 <127303647+devv18@users.noreply.github.com>
2023-12-04 22:51:27 +0530

    Update 0001_initial.py

--- part1/migrations/0001_initial.py
+++ part1/migrations/0001_initial.py
@@ -14,13 +14,13 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Todo',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('timestamp', models.DateTimeField(auto_now_add=True)),
-                ('title', models.CharField(max_length=100)),
-                ('description', models.CharField(max_length=1000)),
-                ('due_date', models.DateField(blank=True, null=True)),
-                ('status', models.CharField(choices=[('OPEN', 'Open'), ('WORKING', 'Working'), ('DONE', 'Done'), ('OVERDUE', 'Overdue')], default='OPEN', max_length=10)),
-                ('tags', models.CharField(blank=True, max_length=200, null=True)),
+('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+('timestamp', models.DateTimeField(auto_now_add=True)),
+('title', models.CharField(max_length=100)),
+('description', models.CharField(max_length=1000)),
+('due_date', models.DateField(blank=True, null=True)),
+('status', models.CharField(choices=[('OPEN', 'Open'), ('WORKING', 'Working'), ('DONE', 'Done'), ('OVERDUE', 'Overdue')], default='OPEN', max_length=10)),
+('tags', models.CharField(blank=True, max_length=200, null=True)),
             ],
         ),
     ]
