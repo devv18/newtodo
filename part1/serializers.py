addc21428c7db9a53c73d126ec7bec1061e00ce4
devv18 <127303647+devv18@users.noreply.github.com>
2023-12-05 09:12:48 +0530

    Update serializers.py

--- part1/serializers.py
+++ part1/serializers.py
@@ -2,7 +2,6 @@ from rest_framework import serializers
 from .models import Todo
 
 
-
 class TodoSerializer(serializers.ModelSerializer):
     tags = serializers.ListField(child=serializers.CharField(), required=False)
 
