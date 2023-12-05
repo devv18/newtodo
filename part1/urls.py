60a015216a55980626084e97ed25532a93d3d0dc
devv18 <127303647+devv18@users.noreply.github.com>
2023-12-05 10:49:50 +0530

    Update urls.py

--- part1/urls.py
+++ part1/urls.py
@@ -11,6 +11,10 @@ urlpatterns = [
     path('create/', TodoCreateView.as_view(), name='todo-create'),
     path('list/', TodoListView.as_view(), name='to-do-list'),
     path('update/<int:pk>/', TodoUpdateView.as_view(), name='todo-update'),
-    path('delete/<int:pk>/', TodoDestroyView.as_view(), name='todo-destroy'),  # Fixed the name here
+    path(
+        'delete/<int:pk>/',
+        TodoDestroyView.as_view(),
+        name='todo-destroy'
+    ),
     path('read/<int:pk>/', TodoRetrieveView.as_view(), name='todo-retrieve'),
 ]
